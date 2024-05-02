aa3eacc1a997ba84187fee5f547bceef1846d9a3
skubed0007 <skub007@proton.me>
2024-05-02 21:02:54 +0530

    Fixed CS Code

--- Try.cs
+++ Try.cs
@@ -5,7 +5,7 @@ public class Program
 	public static void Main()
 	{
 		Console.WriteLine("TO BE ENCRYPTED");
-        Console.WriteLine("You De-Crypted It So The Password..You Know it");
-        Console.WriteLine("THE PASSWORD IS - TRYNEKOLKER");
+        	Console.WriteLine("You De-Crypted It So The Password..You Know it");
+        	Console.WriteLine("THE PASSWORD IS - TRYNEKOLKER");
 	}
 }
